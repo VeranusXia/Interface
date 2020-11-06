6df7fdee5fdd2377e4aaff78fcd2ac7e4867d8b4
xia.weiwei@opg.cn <xia.weiwei@opg.cn>
2020-11-06 16:19:27 +0800

    123
    123

---
+++ AddOns/EJShowSL/EJShowSL.lua
@@ -0,0 +1,21 @@
+﻿local  EJShowSL=CreateFrame("Frame")
+
+EJShowSL.onEJShow = function()
+    EncounterJournalInstanceSelectLootJournalTab:Show()
+    EncounterJournal_TierDropDown_Select(nil, 9)
+end
+
+EJShowSL.hook = function()
+    if (EJShowSL.isHooked or not EncounterJournal) then
+        return nil
+    end
+    hooksecurefunc(EncounterJournal, 'Show', EJShowSL.onEJShow)
+    EJShowSL.onEJShow()
+    EJShowSL.isHooked = 1
+    return nil
+end
+
+EJShowSL:RegisterUnitEvent("UNIT_AURA","player")
+EJShowSL:RegisterUnitEvent("UNIT_SPELLCAST_START","player")
+EJShowSL:SetScript("OnEvent",EJShowSL.hook)
+ 
